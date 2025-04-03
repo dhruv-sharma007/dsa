6ef19d22a594195309aeaad3a494c10f7d87196e
Dhruv <dhruv.freelancerrr@gmail.com>
2025-04-03 11:58:18 +0530

    insertion sort

--- day2/Sorting.java
+++ day2/Sorting.java
@@ -2,11 +2,17 @@
 public class Sorting {
 
     public static void main(String[] args) {
-
+        Sorting sorting = new Sorting();
+        // sorting.selectionSort();
+        // sorting.bubbleSort();
+        sorting.insertionSort();
     }
 
     public void bubbleSort() {
-        int[] arr = {7, 84, 8, 22, 1};
+        int[] arr = {4, 5, 3, 1, 2};
+
+        System.out.println("\nInitial array (Bubble Sort):");
+        printArray(arr);
 
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
@@ -16,29 +22,60 @@ public class Sorting {
                     arr[j + 1] = temp;
                 }
             }
+            System.out.println("After pass " + (i + 1) + ":");
+            printArray(arr);
         }
 
-        for (int i = 0; i < arr.length - 1; i++) {
-
-            System.out.println(arr[i]);
-        }
+        System.out.println("Sorted array (Bubble Sort):");
+        printArray(arr);
     }
 
-    public void selectionSort() {
-        int arr[] = {1, 2, 3, 4, 5};
+    private void selectionSort() {
+        int arr[] = {5, 4, 3, 2, 1};
+
+        System.out.println("Initial array (Selection Sort):");
+        printArray(arr);
 
         for (int i = 0; i < arr.length - 1; i++) {
             int smallest = i;
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[smallest] > arr[j]) {
                     smallest = j;
                 }
-
             }
+
             int temp = arr[smallest];
             arr[smallest] = arr[i];
             arr[i] = temp;
 
+            System.out.println("After pass " + (i + 1) + ":");
+            printArray(arr);
         }
+
+        System.out.println("Sorted array (Selection Sort):");
+        printArray(arr);
+    }
+
+    private void insertionSort() {
+        int arr[] = {1, 4, 5, 7, 2};
+
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = current;
+        }
+        printArray(arr);
+    }
+
+    private void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
